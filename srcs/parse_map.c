1fbd560c4acf6ed546f777a4e1218127f3979593
Aline Queiroz <aqueiroz@c1r8p1.42sp.org.br>
2023-01-23 21:36:26 -0300

    using split

--- srcs/parse_map.c
+++ srcs/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: aqueiroz <aqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 21:05:10 by aqueiroz          #+#    #+#             */
-/*   Updated: 2023/01/23 21:08:13 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/01/23 21:34:40 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	parsing_map(int fd)
 {
-	ft_printf("This is a test!");
+	char	*line;
+	char	**splitted;
+
+	line = get_next_line(fd);
+	splitted = ft_split(line, ' ');
+	ft_printf("%s\n%s", line, splitted[0]);
 	close(fd);
 	return (0);
 }
