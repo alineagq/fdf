f4a3adc2e971f087659577e392939f79134cbb82
Aline Queiroz <aqueiroz@c1r9p4.42sp.org.br>
2023-02-02 20:33:21 -0300

    updated files

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aqueiroz < aqueiroz@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:40:35 by aqueiroz          #+#    #+#             */
-/*   Updated: 2023/01/30 21:06:08 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/02/02 19:56:54 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char *argv[])
 	char	*file;
 
 	file = argv[1];
-	validate_input(argc, file);
+	validate_map(argc, file);
 	parsing_map(file);
 	exit (0);
 }
