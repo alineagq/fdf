55c814a6e78fd9ff9324e4d682e8acc9d9b275b9
Aline Queiroz <aqueiroz@c1r3p1.42sp.org.br>
2023-02-23 19:49:07 -0300

    fixed wrong memory alloc

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aqueiroz < aqueiroz@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:40:35 by aqueiroz          #+#    #+#             */
-/*   Updated: 2023/02/22 23:42:59 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/02/23 19:48:32 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,22 @@ int	main(int argc, char *argv[])
     void    *mlx_ptr;
     void    *win_ptr;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 800, 800, "euzinha");
+	// mlx_ptr = mlx_init();
+	// win_ptr = mlx_new_window(mlx_ptr, 800, 800, "euzinha");
 	file = argv[1];
     cols = get_value_x(file);
     rows = get_value_y(file);
     validate_map(argc, file);
-    map = parsing_map(file, rows, cols);
-    for(int i = 0; i < rows; i*=5)
-    {
-        for(int j = 0; j < cols; j*=5)
-        {
-            mlx_pixel_put(mlx_ptr, win_ptr, rows+i,cols+j , 0xFFFF00);
-            ft_printf("rows %i, cols %i\n", i, j);
-        }
-    }
-    // print_map(map, rows, cols);
+    map = parsing_map(file, rows, --cols);
+    // for(int i = 0; i < rows; i*=5)
+    // {
+    //     for(int j = 0; j < cols; j*=5)
+    //     {
+    //         mlx_pixel_put(mlx_ptr, win_ptr, rows+i,cols+j , 0xFFFF00);
+    //         ft_printf("rows %i, cols %i\n", i, j);
+    //     }
+    // }
+    print_map(map, rows, cols);
     free_map(map, rows);
 	mlx_loop(mlx_ptr);
     exit (0);
