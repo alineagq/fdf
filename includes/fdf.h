ecd1da18102ee72033d100eb38cadb3a3dbffa2a
Aline Queiroz <aqueiroz@c1r3p1.42sp.org.br>
2023-02-23 20:13:14 -0300

    added hooks

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: aqueiroz < aqueiroz@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 03:44:25 by aqueiroz          #+#    #+#             */
-/*   Updated: 2023/02/22 23:32:50 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/02/23 19:56:28 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,15 @@ typedef struct s_point
 	char	*color;
 }	t_point;
 
+typedef struct s_mlx
+{
+	char	*mlx;
+	char	*win;
+	int		cols;
+	int		rows;
+	char	*file;
+}	t_mlx;
+
 void	validate_map(int argc, char *map_name);
 void	validate_input(int argc, char *map_name);
 void	validate_map_type(char *map_name);
@@ -41,10 +50,10 @@ int		get_value_y(char *file);
 int		get_value_x(char *file);
 void	raise_error(char *msg, int err);
 int		ft_size(char *s, char c);
-t_point **parsing_map(char *file, int rows, int cols);
+t_point	**parsing_map(char *file, int rows, int cols);
 int		ft_open(char *file);
 char	*ft_strtok(char *str, const char *c);
-void print_map(t_point **map, int rows, int cols);
-void free_map(t_point **map, int rows);
+void	print_map(t_point **map, int rows, int cols);
+void	free_map(t_point **map, int rows);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,38 +6,41 @@
 /*   By: aqueiroz < aqueiroz@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:40:35 by aqueiroz          #+#    #+#             */
-/*   Updated: 2023/02/23 19:48:32 by aqueiroz         ###   ########.fr       */
+/*   Updated: 2023/02/23 20:12:38 by aqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int my_hook(int keycode, void *param);
+
 int	main(int argc, char *argv[])
 {
-	char    *file;
-	t_point **map;
-	int     cols;
-    int     rows;
-    void    *mlx_ptr;
-    void    *win_ptr;
+	t_point	**map;
+	t_mlx	mlx;
 
-	// mlx_ptr = mlx_init();
-	// win_ptr = mlx_new_window(mlx_ptr, 800, 800, "euzinha");
-	file = argv[1];
-    cols = get_value_x(file);
-    rows = get_value_y(file);
-    validate_map(argc, file);
-    map = parsing_map(file, rows, --cols);
-    // for(int i = 0; i < rows; i*=5)
-    // {
-    //     for(int j = 0; j < cols; j*=5)
-    //     {
-    //         mlx_pixel_put(mlx_ptr, win_ptr, rows+i,cols+j , 0xFFFF00);
-    //         ft_printf("rows %i, cols %i\n", i, j);
-    //     }
-    // }
-    print_map(map, rows, cols);
-    free_map(map, rows);
-	mlx_loop(mlx_ptr);
-    exit (0);
+	mlx.file = argv[1];
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 800, 800, "euzinha");
+	mlx.cols = get_value_x(mlx.file);
+	mlx.rows = get_value_y(mlx.file);
+	validate_map(argc, mlx.file);
+	map = parsing_map(mlx.file, mlx.rows, --mlx.cols);
+	// print_map(map, mlx.rows, mlx.cols);
+	mlx_hook(mlx.win, KeyPress, KeyPressMask, my_hook, NULL);
+	// mlx_key_hook(mlx.win, key_hook, &mlx);
+	free_map(map, mlx.rows);
+	mlx_loop(mlx.mlx);
+	exit (0);
+}
+
+int my_hook(int keycode, void *param)
+{
+    // Handle the keypress event
+	if (keycode == 65307 || keycode == 27)
+		exit(0);
+    printf("Keycode %d pressed!\n", keycode);
+    
+    // Return 0 to indicate that the event was handled successfully
+    return (0);
 }
